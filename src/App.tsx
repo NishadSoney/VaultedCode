cbd97c1871d0b5024ee510b732a81aef89b9d87c
NishadSoney <nishadsoney@gmail.com>
2026-09-18 20:27:14 +0530

    improved1

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+//  importing everything
 import React, { useState, useEffect } from 'react';
 import { Navbar } from './components/Navbar';
 import { UploadSection } from './components/UploadSection';
